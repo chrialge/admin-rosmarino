17f9b2c786c6dc94b9862b75699bd7611468be50
chrialge <chrialge@gmail.com>
2025-01-22 18:52:04 +0100

    add

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -21,7 +21,8 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Usando Vite -->
-    @vite(['resources/js/app.js', 'resources/scss/login.scss'])
+    @vite(['resources/js/app.js'])
+    @vite(['resources/scss/login.scss'])
 
 
 
