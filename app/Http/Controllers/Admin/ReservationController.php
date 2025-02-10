ec160d774f88c84d1a7bd170014ac1bac44060f3
chrialge <chrialge@gmail.com>
2025-02-10 15:30:57 +0100

    add fic problem with front with back of reservation

--- app/Http/Controllers/Admin/ReservationController.php
+++ app/Http/Controllers/Admin/ReservationController.php
@@ -50,11 +50,89 @@ class ReservationController extends Controller
         // creo una nuova prenotaziome
         $reservation = Reservation::create($val_data);
 
-        // richiamo l'oggetto
-        $sendNotfification = new SendNotification();
+        // formato la data della prenotazione
+        $date = date_format(date_create($reservation->date), "d/m/Y");
 
-        // mando la notifica passando l'id della prenotazione
-        $sendNotfification->send($reservation->id);
+        // formato l'ora della prenotazione
+        $time = date_format(date_create($reservation->hour_reservation), "H:i");
+
+        // prendo il il token di telegram
+        $token = env("TELEGRAM_BOT_TOKEN");
+
+        // url per mandare il messaggio su telegram
+        $apiUrl = "https://api.telegram.org/bot{$token}/sendMessage";
+
+        // prendo l'id del contatto
+        $chatId = env("TELEGRAM_CHAT_ID");
+
+        // salvo il messaggio 
+        $botMessage = "Prenotazione di: $reservation->customer_name $reservation->customer_last_name alle ore $time il giorno $date per $reservation->person persone. Il suo contatto: $reservation->customer_telephone";
+
+        // salvo i due bottoni che voglio nel messagio
+        $keyboard = json_encode([
+            "inline_keyboard" => [
+                [
+                    [
+                        "text" => "conferma",
+                        "url" => "http://127.0.0.1:8000/api/confirm-reservation/{$reservation->id}"
+                    ],
+                    [
+                        "text" => "rifiuta",
+                        "url" => "http://127.0.0.1:8000/api/reject-reservation/{$reservation->id}"
+                    ],
+                ]
+            ]
+        ]);
+
+        // setto i parametri per la richiesta API
+        $parameters = array(
+            "chat_id" => $chatId,
+            "text" => $botMessage,
+            "parse_mode" => 'Markdown',
+            'reply_markup' => $keyboard
+        );
+
+        // inizializo una chiamata
+        $ch = curl_init();
+
+        // setto l'url
+        curl_setopt($ch, CURLOPT_URL, $apiUrl);
+
+        // setto con il numero di parametri
+        curl_setopt($ch, CURLOPT_POST, count($parameters));
+
+        // setto con i dati
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
+
+        // setto che voglio ricevere in cambio
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // esaguo la chiamta
+        $result = curl_exec($ch);
+
+        // chiudo la chiamat
+        curl_close($ch);
+
+
+        // prendo i risultati 
+        $result = json_decode($result)->result;
+
+        // salvo l'id del messaggio
+        $message_id = $result->message_id;
+
+        // setto per tutti campi della prenotazione
+        $newReservation['customer_name'] = $reservation->customer_name;
+        $newReservation['customer_last_name'] = $reservation->customer_last_name;
+        $newReservation['customer_telephone'] = $reservation->customer_telephone;
+        $newReservation['customer_email'] = $reservation->customer_email;
+        $newReservation['date'] = $reservation->date;
+        $newReservation['hour_reservation'] = $reservation->hour_reservation;
+        $newReservation['person'] = $reservation->person;
+        $newReservation['message_id'] = $message_id;
+        $newReservation['state'] = 'attesa';
+
+        // aggiorni i dati per la prenotazione
+        Reservation::where('id', $reservation['id'])->update($newReservation);
 
         // rispondo con un messaggio di successo
         return response()->json([
