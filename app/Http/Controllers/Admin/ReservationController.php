26e96de2c021b458a147b8723d092e092b519c4a
chrialge <chrialge@gmail.com>
2025-02-10 15:59:45 +0100

    add fix

--- app/Http/Controllers/Admin/ReservationController.php
+++ app/Http/Controllers/Admin/ReservationController.php
@@ -36,112 +36,41 @@ class ReservationController extends Controller
      */
     public function store(StoreReservationRequest $request)
     {
-        //prendo tutti i dati validati
+
         $val_data = $request->validated();
 
-        // aggiungo lo stato di attessa
         $val_data['state'] = 'attesa';
 
-        // 
         $val_data['date'] = date_format(date_create($val_data['date']), 'Y-m-d');
 
         $val_data['hour_reservation'] = date_format(date_create($val_data['hour_reservation']), 'H:i');
 
-        // creo una nuova prenotaziome
         $reservation = Reservation::create($val_data);
 
-        // formato la data della prenotazione
-        $date = date_format(date_create($reservation->date), "d/m/Y");
+        // richiamo l'oggetto
+        $sendNotfification = new SendNotification();
 
-        // formato l'ora della prenotazione
-        $time = date_format(date_create($reservation->hour_reservation), "H:i");
-
-        // prendo il il token di telegram
-        $token = env("TELEGRAM_BOT_TOKEN");
-
-        // url per mandare il messaggio su telegram
-        $apiUrl = "https://api.telegram.org/bot{$token}/sendMessage";
-
-        // prendo l'id del contatto
-        $chatId = env("TELEGRAM_CHAT_ID");
-
-        // salvo il messaggio 
-        $botMessage = "Prenotazione di: $reservation->customer_name $reservation->customer_last_name alle ore $time il giorno $date per $reservation->person persone. Il suo contatto: $reservation->customer_telephone";
-
-        // salvo i due bottoni che voglio nel messagio
-        $keyboard = json_encode([
-            "inline_keyboard" => [
-                [
-                    [
-                        "text" => "conferma",
-                        "url" => "https://backrosmarino.org/api/confirm-reservation/{$reservation->id}"
-                    ],
-                    [
-                        "text" => "rifiuta",
-                        "url" => "https://backrosmarino.org/api/reject-reservation/{$reservation->id}"
-                    ],
-                ]
-            ]
-        ]);
-
-        // setto i parametri per la richiesta API
-        $parameters = array(
-            "chat_id" => $chatId,
-            "text" => $botMessage,
-            "parse_mode" => 'Markdown',
-            'reply_markup' => $keyboard
-        );
-
-        // inizializo una chiamata
-        $ch = curl_init();
-
-        // setto l'url
-        curl_setopt($ch, CURLOPT_URL, $apiUrl);
-
-        // setto con il numero di parametri
-        curl_setopt($ch, CURLOPT_POST, count($parameters));
-
-        // setto con i dati
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
-
-        // setto che voglio ricevere in cambio
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        // esaguo la chiamta
-        $result = curl_exec($ch);
+        // mando la notifica passando l'id della prenotazione
+        $sendNotfification->send($reservation->id);
 
 
-
-        // chiudo la chiamat
-        curl_close($ch);
-
-
-        // prendo i risultati 
-        $result = json_decode($result)->result;
-
-        // salvo l'id del messaggio
-        $message_id = $result->message_id;
-
-        // setto per tutti campi della prenotazione
-        $newReservation['customer_name'] = $reservation->customer_name;
-        $newReservation['customer_last_name'] = $reservation->customer_last_name;
-        $newReservation['customer_telephone'] = $reservation->customer_telephone;
-        $newReservation['customer_email'] = $reservation->customer_email;
-        $newReservation['date'] = $reservation->date;
-        $newReservation['hour_reservation'] = $reservation->hour_reservation;
-        $newReservation['person'] = $reservation->person;
-        $newReservation['message_id'] = $message_id;
-        $newReservation['state'] = 'attesa';
-
-        // aggiorni i dati per la prenotazione
-        Reservation::where('id', $reservation['id'])->update($newReservation);
-
-        // rispondo con un messaggio di successo
         return response()->json([
             'success' => true,
-            'response' => $result,
+            'response' => $val_data,
         ]);
 
+        // //prendo tutti i dati validati
+        // $dirty_data = $request->validated();
+
+        // // aggiungo lo stato di attessa
+        // $dirty_data['state'] = 'attesa';
+
+        // // creo una nuova prenptaziome
+        // $reservation = Reservation::create($dirty_data);
+
+
+
+        // // rispondo con un messaggio di successo
         // return response()->json([
         //     'success' => true,
         //     'response' => "La tua prenotazione e arrivata",
